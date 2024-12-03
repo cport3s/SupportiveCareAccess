f9bb43a682fcf5c75cb58f50dd63df3f4da53d70
Carlos Portes <carlosportes.c@gmail.com>
2024-12-03 15:37:29 -0500

    Update sca_main.py
    Testing display settings for main container content.

--- sca_main.py
+++ sca_main.py
@@ -10,8 +10,9 @@ import plotly.express as px
 from plotly.subplots import make_subplots
 from datetime import datetime, timedelta
 from dash_styles import nav_bar
-from sca_functions import get_layout
+from sca_functions import suppcare_header
 from classes import schemaList, dbCredentials
+from dash_styles import SuppCareBanner
 import logging
 
 db_address = dbCredentials.db_address
@@ -38,7 +39,70 @@ main_app.layout = html.Div(
         # App container, will be updated based on url
         html.Div(
             id='sca_main_container',
-            style=nav_bar.CONTENT_STYLE
+            style=nav_bar.CONTENT_STYLE,
+            children = [
+                suppcare_header(),
+                html.H2(
+                    children = ['Facility Statistics'],
+                    style = SuppCareBanner.orange_style
+                ),
+                html.Div(
+                    id = 'facility_dropdown_container',
+                    children = [
+                        dcc.Dropdown(
+                            id = 'state_dropdown',
+                            #options = [{'label': v[-2], 'value': k} for k,v in schemaList.schemaDict.items()],
+                            #value = 'All'
+                        ),
+                        dcc.Dropdown(
+                            id = 'facility_dropdown'
+                        ),
+                        dcc.Dropdown(
+                            id = 'date_filter',
+                            options = [
+                                {'label': '7 days', 'value': 7},
+                                {'label': '30 days', 'value': 30},
+                                {'label': '90 days', 'value': 90},
+                                {'label': '360 days', 'value': 360},
+                                {'label': 'All', 'value': ''}
+                            ],
+                            value = ''
+                        )
+                    ]
+                ),
+                html.Div(
+                    id = 'facility_upload_log_graph_container',
+                    style = {'display': 'none'},
+                    children = [
+                        dcc.Graph(
+                            id = 'facility_upload_log_graph'
+                        )
+                    ]
+                ),
+                html.Div(
+                    id = 'global_facility_upload_log_graph_container',
+                    style = {'display': 'block'},
+                    children = [
+                        dcc.Graph(
+                            id = 'global_facility_upload_log_graph'
+                        )
+                    ]
+                ),
+                html.Div(
+                    id = 'pending_logs_sunburst_chart_container',
+                    style = {'display': 'block'},
+                    children = [
+                        dcc.Graph(
+                            id = 'pending_logs_sunburst_chart'
+                        )
+                    ]
+                ),
+                dcc.Interval(
+                    id = 'update_interval',
+                    interval = 3000*1000,
+                    n_intervals = 0
+                )
+            ]
         ),
         dcc.Interval(
             id = 'update_interval',
@@ -50,18 +114,22 @@ main_app.layout = html.Div(
 
 #-------------------------------------------CALLBACKS
 @main_app.callback(
-    Output('sca_main_container', 'children'),
+    Output('sca_main_container', 'style'),
     Input('current_url', 'pathname')
 )
 def render_container(pathname):
     # sca_main_container div content will be updated based on URL
-    layout_return = get_layout(pathname)
-    return layout_return
+    layout_return = html.P('filler')
+    if pathname == '/fac_stats':
+        return {'display': 'inline'}
+    else:
+        return {'display': 'hidden'}
 
 # Callback to populate the state dropdown
 @main_app.callback(
     Output('state_dropdown', 'options'),
-    Input('current_url', 'pathname')
+    Input('current_url', 'pathname'),
+    prevent_initial_call=True
     )
 def populate_facility_dropdown(pathname):
     if pathname == '/fac_stats':
